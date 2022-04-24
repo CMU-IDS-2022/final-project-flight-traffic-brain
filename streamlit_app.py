34b5eae6cfda430234fe52ae442140ee0ba5dff5
Shuting (Tina) Feng <57819171+TINAF1109@users.noreply.github.com>
2022-04-24 18:08:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -333,7 +333,7 @@ else:
         if de != "Virgin Islands":
             df_pred[f'd{de}'] = 1
         else:
-            df_pred['dU.S. Virgin Isands']
+            df_pred['dU.S. Virgin Isands']=1
         
         if season!='Spring':
             df_pred[quarter_dic[season]] = 1
