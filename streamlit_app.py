bb5ac5b3ca25c0185b0d8e21abf118eec46ee68b
Shuting (Tina) Feng <57819171+TINAF1109@users.noreply.github.com>
2022-04-27 13:06:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -604,9 +604,9 @@ else:
     
     st.sidebar.title("Note")
 
-    st.sidebar.write("This flight price analysis consists of four components.\
-        The first component is a flight customization section with predicted price range \
-        against historical price distribution\
-        The second component presents a table of customized historical flights of interest. \
-        The third component displays the historical average flight price by route and airline company\
-        The fourth component...")
+    st.sidebar.write("This flight price analysis consists of four parts.\
+        The first part is a flight customization section with predicted price range \
+        against historical price distribution.\
+        The second part presents a table of customized historical flights of interest. \
+        The third part displays the historical average flight price by route and airline company.\
+        The last part...")
