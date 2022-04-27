afda5d70f74f269712496aab691cd37c7c9c0001
Shuting (Tina) Feng <57819171+TINAF1109@users.noreply.github.com>
2022-04-27 12:39:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,14 +106,27 @@ if menu_selection == 'Introduction':
     col3.markdown("<h1 style='text-align: left; color: #072F5F;'>Flight Traffic Brain</h1>",
             unsafe_allow_html=True)
 
-    col1, col2, col3 = st.columns([1, 6, 1])   
+    
     text = "<p style='font-size:18px'>Nowadays, air traffic control has become a complicated task as there are\
     more and more flights and airlines. There has also been rising cases of flight delays possibly due to poor\
     management and massive volume of traffic. While air traffic is important to manage from the perspective of\
     airports and airlines, flight prices are crucial for customers who usually make decisions of their travel\
     plans based on them. In this project we hope to help airports better manage airlines and control airline\
-    traffic and passengers make wiser decisions about airline flights. </p>"
-    col2.write(text, unsafe_allow_html=True)
+    traffic and passengers make wiser decisions about airline flights.</p>" 
+    st.write(text, unsafe_allow_html=True)
+    
+    text = "<p style='font-size:18px'>A real-time map of flights with interactive information such as speed and altitude can help the specialists\
+    to make better decisions. Meanwhile, an interactive network graph that shows the connections between airports and\
+    flights can also improve the handling of dependencies among the traffic. A data visualization section can also enable users\
+    to analyze the delay time of different flights in real time and in more detail. By filtering the flight according to their\
+    departure airport, the users can not only view the delay time of different flights, but also have a high-level overview of\
+    the delay information of flights of different airlines. This information will help airport specialists to better communicate\
+    with the airports and passengers, and make better decisions in terms of resource distribution. In addition, a machine learning model using\
+    historical data to predict flight price can help passengers estimate the potential fare of flight of their\
+    interest. It can also allow them to compare different flight prices by modifying parameters of interest,\
+    thus helping optimize their travel plan.</p>"
+    st.write(text, unsafe_allow_html=True)
+    
     
 
     
